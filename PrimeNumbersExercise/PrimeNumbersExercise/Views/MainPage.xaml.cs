81baf7cba9bcf590d02d2624623a908e632dcb8d
Martin Lenart <martin_lenart@icloud.com>
2022-01-20 16:17:51 +0100

    Add project files.

---
+++ PrimeNumbersExercise/PrimeNumbersExercise/Views/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PrimeNumbersExercise.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : FlyoutPage
+    {
+        public MainPage(string Message, DateTime time)  //Paremeters used to demonstrate parameter passing
+        {
+            InitializeComponent();
+            FlyoutPage.ListView.ItemSelected += ListView_ItemSelected;
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                FlyoutLayoutBehavior = FlyoutLayoutBehavior.Popover;
+            }
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MainPageFlyoutMenuItem;
+            if (item == null)
+                return;
+
+            var page = (Page)Activator.CreateInstance(item.TargetType);
+            page.Title = item.Title;
+
+            Detail = new NavigationPage(page);
+            IsPresented = false;
+
+            FlyoutPage.ListView.SelectedItem = null;
+        }
+    }
+}
---
+++ PrimeNumbersExercise/PrimeNumbersExercise/Views/MainPageFlyout.xaml.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PrimeNumbersExercise.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageFlyout : ContentPage
+    {
+        public ListView ListView;
+
+        public MainPageFlyout()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageFlyoutViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class MainPageFlyoutViewModel
+        {
+            public ObservableCollection<MainPageFlyoutMenuItem> MenuItems { get; set; }
+
+            public MainPageFlyoutViewModel()
+            {
+                MenuItems = new ObservableCollection<MainPageFlyoutMenuItem>(new[]
+                {
+                    new MainPageFlyoutMenuItem { Id = 0, Title = "About Primes", TargetType=typeof(AboutPage) },
+                    new MainPageFlyoutMenuItem { Id = 1, Title = "Debug Console", TargetType=typeof(ConsolePage) },
+                });
+            }
+        }
+    }
+}
